928fb10493da52315a3c844388216c91a4dffcff
Adam Szlachta <adam.szlachta@gmail.com>
2019-02-12 02:11:50 +0100

    Json parsing examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
 import Workshop.LensExamples as LensExamples
+import Workshop.JsonExamples as JsonExamples
+import Workshop.Json as Json
 
 main :: IO ()
 main = do
   LensExamples.run
+  JsonExamples.run
+  Json.run
---
+++ src/Workshop/Json.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Workshop.Json where
+
+import Data.List (intercalate)
+import Data.Aeson (FromJSON, ToJSON, eitherDecode)
+import GHC.Generics (Generic)
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+
+-- hints:
+-- type FilePath = String
+-- readFile :: FilePath -> IO ByteString
+-- simpleHttp :: MonadIO m => String -> m ByteString
+-- decode :: FromJSON a => ByteString -> Maybe a
+-- eitherDecode :: FromJSON a => ByteString -> Either String a
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+-- fmap  :: Functor f => (a -> b) -> f a -> f b
+
+
+-- TASK 1
+data ToDo = ToDo deriving Show
+
+-- TASK 2
+decodeToDos :: B.ByteString -> Either String [ToDo]
+decodeToDos = undefined
+
+-- TASK 3
+getToDo :: Int -> IO (Either String ToDo)
+getToDo = undefined
+-- from: https://jsonplaceholder.typicode.com/todos/
+
+-------------
+
+-- TASK 4
+data Person = Person deriving Show
+
+-- TASK 5
+jsonFile :: FilePath
+jsonFile = undefined
+-- look in `data` folder
+
+-- TASK 6
+getJsonByteString :: IO B.ByteString
+getJsonByteString = undefined
+
+-- TASK 7
+decodePersons :: B.ByteString -> Either String [Person]
+decodePersons = undefined
+
+-- TASK 8
+getPersons :: IO (Either String [Person])
+getPersons = undefined
+
+-------------
+
+run :: IO ()
+run = do
+  -- personsE <- getPersons
+  -- case personsE of
+  --   Left err -> putStrLn err
+  --   Right persons -> print persons
+  -- todoE <- getToDo 1
+  -- case todoE of
+  --   Left err -> putStrLn err
+  --   Right todo -> print todo
+  putStrLn "done"
